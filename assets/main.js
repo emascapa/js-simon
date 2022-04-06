e73ffc19815c8b530fbb0f4a6f6e24d6e0834ed1
emascapa <emascapa@gmail.com>
2022-04-06 14:43:01 +0200

    Initial Commit

---
+++ assets/main.js
@@ -0,0 +1,72 @@
+
+const numbersRowElement = document.getElementById('numbers_row');
+
+const inputRowElement = document.getElementById('input_row');
+
+const pcNumbers = createRandomNumbersArray(5, 100);
+
+for (let i = 0; i < pcNumbers.length; i++) {
+
+    let htmlContent = `
+        <div class="col">
+        <div class="card text-center p-4 display-4">${pcNumbers[i]}</div>
+        </div>
+    `;
+
+    numbersRowElement.insertAdjacentHTML('beforeend', htmlContent);
+}
+
+setTimeout(hideNumberElements('#numbers_row .card'), 50000);
+
+
+
+function hideNumberElements(querySelectorString) {
+
+    const selectedElements = document.querySelectorAll(querySelectorString);
+
+    console.log(selectedElements);
+
+    for (let i = 0; i < selectedElements.length; i++) {
+        selectedElements[i].innerHTML = `<i class="fa-solid fa-circle-question"></i>`;
+
+        console.log(`fatto ${i}`);
+    }
+
+}
+
+
+
+
+//creatore di vettore di numeri casuali
+function createRandomNumbersArray(arrayDimension, maxValue) {
+
+    const outputArray = [];
+
+    while (outputArray.length !== arrayDimension) {
+        const randomNumber = Math.floor(Math.random() * maxValue) + 1;
+
+        if (!outputArray.includes(randomNumber)) {
+            outputArray.push(randomNumber);
+        }
+    }
+
+    return outputArray;  
+}
+
+
+//funzione per stabilire il punteggio tra numeri comparsi e numeri inseriti
+function compareArray(array1, array2) {
+
+    let counter = 0;
+
+    if (array1.length === array2.length) {
+        for (let i = 0; i < array1.length; i++) {
+            if (array2.includes(array1[i])) {
+
+                counter++;
+            }
+        }
+    }
+
+    return counter;
+}
